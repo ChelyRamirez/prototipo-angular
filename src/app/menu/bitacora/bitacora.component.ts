a1e05cdcffc0dc0be26b78305b1377c6fa06335e
Hector Ramirez <hectorivan.ram370@gmail.com>
2021-07-01 09:53:44 -0500

    ya funciona

--- src/app/menu/bitacora/bitacora.component.ts
+++ src/app/menu/bitacora/bitacora.component.ts
@@ -9,11 +9,7 @@ import { GlobalService } from '../../services/global.service';
 export class BitacoraComponent implements OnInit {
   
   bit: any = [];
-<<<<<<< HEAD
-  public re;
-=======
   public registros; 
->>>>>>> 4e6dd886792019351f66cb2af17fb642f0045fdd
   constructor(
     private bitacora: GlobalService
   ) { }
@@ -26,11 +22,7 @@ export class BitacoraComponent implements OnInit {
     return this.bitacora.verBitacora().subscribe(
       res => {
         this.bit = res;
-<<<<<<< HEAD
-        return this.re = this.bit.resultado;
-=======
          this.registros = this.bit.resultado;
->>>>>>> 4e6dd886792019351f66cb2af17fb642f0045fdd
       },
       err => console.log(err)
     )
