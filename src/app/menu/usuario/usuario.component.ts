63d0701f83387b12e0977dc4d85ecbe3feb445b3
Araceli <ramirezluz033@gmail.com>
2021-07-06 17:53:57 -0500

    actualizacion

--- src/app/menu/usuario/usuario.component.ts
+++ src/app/menu/usuario/usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
 import { environment } from '../../../environments/environment';
-
+declare var H: any;
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
@@ -8,6 +9,7 @@ import { environment } from '../../../environments/environment';
 })
 export class UsuarioComponent implements OnInit {
   public rutaIMG = environment.RUTA_IMAGEN;
+  
   constructor() { }
 
   ngOnInit(): void {
